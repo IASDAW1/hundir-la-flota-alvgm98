89d601e55ee52cae21c89aa460acef18950bf5c6
alvgm98 <alvgm1998@gmail.com>
2024-02-13 00:11:34 +0100

    Perfeccionado el metodo comprobarBarco() Implementado el metodo crearTableroAleatorio() Menu de inicio del juego en el Main

--- hundirlaflota/src/main/java/hundirlaflota/Main.java
+++ hundirlaflota/src/main/java/hundirlaflota/Main.java
@@ -3,10 +3,31 @@ package hundirlaflota;
 import hundirlaflota.model.Tablero;
 
 public class Main {
-    static Tablero tableroA = new Tablero();
-    static Tablero tableroB = new Tablero();
+    static Tablero tableroJugador = new Tablero();
+    static Tablero tableroMaquina = new Tablero();
+
     public static void main(String[] args) {
 
-        tableroA.imprimirTableroAliado();
+        System.out.println("""
+                =======================================================================
+                ---JUEGO HUNDIR LA FLOTA---
+                OBJETIVO DEL JUEGO:
+                - Hundir los Barcos del rival antes de que hunda los tuyos.
+                REGLAS DEL JUEGO:
+                - Cada jugador constará de 4 Barcos:
+                    # 1 Portaaviones.
+                    # 1 Acorazado.
+                    # 1 Submarino.
+                    # 1 Destructor.
+                LEYENDA:
+                * -> Coordenada sin atacar.
+                A -> Coordenada ataca siendo esta agua.
+                F -> Coordenada ataca siendo esta un barco.
+                =======================================================================""");
+
+        // tableroJugador.imprimirTableroAliado();
+
+        tableroMaquina.crearTableroAleatorio();
+        tableroMaquina.imprimirTableroAliado();
     }
 }
--- hundirlaflota/src/main/java/hundirlaflota/model/Tablero.java
+++ hundirlaflota/src/main/java/hundirlaflota/model/Tablero.java
@@ -1,12 +1,13 @@
 package hundirlaflota.model;
 
-// import java.util.List;
-
 import hundirlaflota.model.barcos.Barco;
+import hundirlaflota.model.barcos.Portaaviones;
+import hundirlaflota.model.barcos.Acorazado;
+import hundirlaflota.model.barcos.Submarino;
+import hundirlaflota.model.barcos.Destructor;
 
 public class Tablero {
    private char[][] tablero;
-   // private List<Barco> barcos;
 
    public Tablero() {
       tablero = new char[10][10];
@@ -34,8 +35,10 @@ public class Tablero {
       }
    }
 
-   // METODO PARA COMPROBAR SI EL BARCO CABE, SUPERPONE A OTRO O SE ENCUENTRA
-   // ADYACENTE A OTRO.
+   /*
+    * METODO PARA COMPROBAR SI EL BARCO CABE, SUPERPONE A OTRO
+    * O SE ENCUENTRA ADYACENTE A OTRO.
+    */
    public boolean comprobarBarco(Barco barco) {
       // Descarta error de que el barco no cabe en el tablero.
       if ((barco.getDireccion() == 'H' && barco.getCoordInicialX() + barco.getLongitud() > 10)
@@ -54,21 +57,42 @@ public class Tablero {
       int aux = i + barco.getLongitud() + 1;
 
       for (; i <= aux; i++) {
-         // Para evitar error de "Fuera de limites".
-         if (i < 0 || i > 10) {
+         // Para evitar error de "Fuera de limites" y comparaciones innecesarias
+         // ya que según las reglas el barco puede estar en los bordes del tablero.
+         if (i < 0 || i >= 10) {
             continue;
          }
+         // HORIZONTAL
          if (barco.getDireccion() == 'H') {
+            /*
+             * Para evitar error de "Fuera de limites" compara primero que el indice no se
+             * salga de los limites.
+             * (Si no cumple la primera parte de la puerta logica AND no pasa a la segunda
+             * parte)
+             */
+            // Compará con la posicion que ocupará y la adyacente.
             if (tablero[i][barco.getCoordInicialY()] != '*'
-                  || tablero[i][barco.getCoordInicialY() + 1] != '*'
-                  || tablero[i][barco.getCoordInicialY() - 1] != '*') {
+                  || (!((barco.getCoordInicialY() + 1) >= 10) && tablero[i][barco.getCoordInicialY() + 1] != '*')
+                  || (!((barco.getCoordInicialY() - 1) < 0) && tablero[i][barco.getCoordInicialY() - 1] != '*')) {
                System.out.println("El barco no puede estar adyacente ni superponer a otro.");
                return false;
             }
-         } else {
+         }
+         // VERICAL
+         if (barco.getDireccion() == 'V') {
+            /*
+             * Para evitar error de "Fuera de limites" compara primero que el indice no se
+             * salga de los limites.
+             * (Si no cumple la primera parte de la puerta logica AND no pasa a la segunda
+             * parte)
+             */
+            if ((barco.getCoordInicialX() + 1) >= 10 || (barco.getCoordInicialX() - 1) < 0) {
+               continue;
+            }
+            // Compará con la posicion que ocupará y la adyacente.
             if (tablero[barco.getCoordInicialX()][i] != '*'
-                  || tablero[barco.getCoordInicialX()][i] != '*'
-                  || tablero[barco.getCoordInicialX()][i] != '*') {
+                  || (!((barco.getCoordInicialX() + 1) >= 10) && tablero[barco.getCoordInicialX() + 1][i] != '*')
+                  || (!((barco.getCoordInicialY() - 1) < 0) && tablero[barco.getCoordInicialX() - 1][i] != '*')) {
                System.out.println("El barco no puede estar adyacente ni superponer a otro.");
                return false;
             }
@@ -99,6 +123,86 @@ public class Tablero {
             System.out.print(tablero[i][j]);
          }
          System.out.println();
+      }
+   }
+
+   // IMPRIME EL TABLERO PARA EL RIVAL.
+   public void imprimirTableroRival() {
+      for (int i = -1; i < 10; i++) {
+         for (int j = -1; j < 10; j++) {
+            System.out.print(" ");
+            if (i < 0 && j < 0) {
+               System.out.print(" ");
+               continue;
+            }
+            if (i < 0) {
+               System.out.print(j);
+               continue;
+            }
+            if (j < 0) {
+               System.out.print(i);
+               continue;
+            }
+
+            // Evita imprimir los bacos del rival.
+            if (tablero[i][j] != 'F' && tablero[i][j] != 'A') {
+               System.out.print('*');
+               continue;
+            }
+            System.out.print(tablero[i][j]);
+         }
+         System.out.println();
+      }
+   }
+
+   public void crearTableroAleatorio() {
+      // Portaaviones
+      while (true) {
+         int coordInicialX = (int) (Math.random() * 10);
+         int coordInicialY = (int) (Math.random() * 10);
+         char direccion = Math.random() < 0.5 ? 'H' : 'V';
+
+         Barco poortaaviones = new Portaaviones(direccion, coordInicialX, coordInicialY);
+         if (comprobarBarco(poortaaviones)) {
+            colocarBarco(poortaaviones);
+            break;
+         }
+      }
+      // Acorazado
+      while (true) {
+         int coordInicialX = (int) (Math.random() * 10);
+         int coordInicialY = (int) (Math.random() * 10);
+         char direccion = Math.random() < 0.5 ? 'H' : 'V';
+
+         Barco poortaaviones = new Acorazado(direccion, coordInicialX, coordInicialY);
+         if (comprobarBarco(poortaaviones)) {
+            colocarBarco(poortaaviones);
+            break;
+         }
+      }
+      // Submarino
+      while (true) {
+         int coordInicialX = (int) (Math.random() * 10);
+         int coordInicialY = (int) (Math.random() * 10);
+         char direccion = Math.random() < 0.5 ? 'H' : 'V';
+
+         Barco poortaaviones = new Submarino(direccion, coordInicialX, coordInicialY);
+         if (comprobarBarco(poortaaviones)) {
+            colocarBarco(poortaaviones);
+            break;
+         }
+      }
+      // Destructor
+      while (true) {
+         int coordInicialX = (int) (Math.random() * 10);
+         int coordInicialY = (int) (Math.random() * 10);
+         char direccion = Math.random() < 0.5 ? 'H' : 'V';
+
+         Barco poortaaviones = new Destructor(direccion, coordInicialX, coordInicialY);
+         if (comprobarBarco(poortaaviones)) {
+            colocarBarco(poortaaviones);
+            break;
+         }
       }
    }
 }
